a088b36fa342735b41a8178065d477d42673b259
Irwing Naranjo <khalisser@gmail.com>
2025-06-20 11:39:45 -0400

    Refactor Dictation component: improve layout and styling for better responsiveness and user experience

--- resources/js/pages/apps/dictation/index.tsx
+++ resources/js/pages/apps/dictation/index.tsx
@@ -91,22 +91,22 @@ export default function Dictation({ api_requests }: { api_requests: { request_co
                     <div className="flex w-full items-center justify-between">
                         <div className="flex w-full flex-col justify-between text-balance">
                             <h1 className="text-3xl font-bold">Dictation</h1>
-                            <p className="text-lg text-muted-foreground">Listen to the audio and write what you hear.</p>
+                            <p className="text-lg text-muted-foreground hidden md:block">Listen to the audio and write what you hear.</p>
                         </div>
 
-                        <div className="flex w-full items-center justify-end gap-2 text-balance">
-                            <p className="text-lg text-muted-foreground">AI Attempts</p>
-                            <div className="flex justify-between rounded-full bg-lime-500 p-2 px-4 font-bold text-balance text-white">
+                        <div className="flex w-full items-center justify-end gap-2 text-balance text-sm md:text-lg">
+                            <p className="text-muted-foreground">AI Attempts</p>
+                            <div className="flex justify-between rounded-full bg-lime-500 p-1 md:p-2 px-3 md:px-4 font-bold text-balance text-white">
                                 <p>{requests}/2</p>
                             </div>
                         </div>
                     </div>
 
-                    <div className="grid h-full w-full grid-cols-2 gap-4">
-                        <div className="flex flex-col items-center justify-center border-r text-primary px-12 gap-4">
-                            <h2 className="text-xl font-bold">Listen to the Audio and write what you hear</h2>
-                            <p className="text-lg text-muted-foreground">
-                            You can repeat the audio as many times as you like. You can also watch the original text, but it's best to try writing it before watching it.
+                    <div className="grid h-full w-full grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="flex flex-col items-center justify-start md:justify-center md:border-r text-primary md:px-12 gap-4 mt-6 md:mt-0">
+                            <h2 className="text-md md:text-xl font-bold">Listen to the Audio and write what you hear</h2>
+                            <p className="text-lg text-muted-foreground hidden md:block">
+                                You can repeat the audio as many times as you like. You can also watch the original text, but it's best to try writing it before watching it.
                             </p>
                             {audioError && (
                                 <div className="p-3 bg-red-100 text-red-700 rounded-lg mb-4">
@@ -114,7 +114,7 @@ export default function Dictation({ api_requests }: { api_requests: { request_co
                                 </div>
                             )}
                             {dictation?.audio && (
-                                <div className="flex flex-col gap-6 w-full">
+                                <div className="flex flex-col gap-4 md:gap-6 w-full">
                                     <audio
                                         ref={audioRef}
                                         controls
@@ -131,7 +131,7 @@ export default function Dictation({ api_requests }: { api_requests: { request_co
                                         </Button>
                                     </div>
                                     {dictation && (
-                                        <div className={cn("bg-gray-50 rounded-lg p-6 transition-all duration-300", showText && dictation ? 'opacity-100' : 'opacity-0')}>
+                                        <div className={cn("bg-gray-50 rounded-lg p-6 transition-all duration-300", showText && dictation ? 'opacity-100 block md:block' : 'opacity-0 hidden md:block')}>
                                             <div className="text-gray-700 text-balance">
                                                 {dictation.text}
                                             </div>
@@ -142,7 +142,7 @@ export default function Dictation({ api_requests }: { api_requests: { request_co
                         </div>
 
                         <div className="flex h-full w-full flex-col items-end justify-between gap-4">
-                            <div className="w-full max-w-2xl text-center text-xl text-balance px-12 h-full flex items-center justify-center">
+                            <div className="w-full md:max-w-2xl text-center text-base md:text-xl text-balance md:px-12 h-full flex items-center justify-center">
                                 {requests > 2 ? 'Alcanzaste el límite de intentos para hoy. Intenta mañana.' : feedback}
                             </div>
 
@@ -152,7 +152,7 @@ export default function Dictation({ api_requests }: { api_requests: { request_co
                                         <textarea
                                             value={userInput}
                                             onChange={(e) => setUserInput(e.target.value)}
-                                            className="w-full h-64 p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none"
+                                            className="w-full h-32 md:h-64 p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none"
                                             placeholder="Type what you hear here..."
                                         />
                                     </div>
@@ -160,14 +160,14 @@ export default function Dictation({ api_requests }: { api_requests: { request_co
                                         <div className="flex text-balance gap-4">
                                             <button
                                                 onClick={handleClear}
-                                                className="min-h-[67px] cursor-pointer px-6 py-2 text-xl font-bold hover:text-orange-500"
+                                                className="min-h-[57px] md:min-h-[67px] cursor-pointer px-6 py-2 text-md md:text-xl font-bold hover:text-orange-500"
                                             >
-                                                Reset my answer
+                                                Reset answer
                                             </button>
                                             <button
                                                 onClick={loadingFeedback ? undefined : handleCheckAnswer}
                                                 disabled={loadingFeedback}
-                                                className="min-h-[67px] flex items-center justify-center gap-2 min-w-48 cursor-pointer border-1 px-6 py-2 text-xl font-bold hover:border-lime-500 hover:text-lime-500"
+                                                className="min-h-[57px] md:min-h-[67px] flex items-center justify-center gap-2 min-w-48 cursor-pointer border-1 px-6 py-2 text-md md:text-xl font-bold hover:border-lime-500 hover:text-lime-500"
                                             >
                                                 {loadingFeedback ? <Loader2 className="animate-spin" /> : ''}
                                                 {loadingFeedback ? 'Checking...' : 'Check my answer'}
@@ -178,8 +178,6 @@ export default function Dictation({ api_requests }: { api_requests: { request_co
                             </div>
                         </div>
                     </div>
-
-                    <div className="absolute right-6 bottom-6 w-[120px]"></div>
                 </div>
             </div>
         </AppLayout>
