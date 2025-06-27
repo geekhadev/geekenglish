de5ac5dd5b4f03e633a286786e3057efc0aff564
Irwing Naranjo <khalisser@gmail.com>
2025-06-27 09:25:24 -0400

    Refactor favicon and update page titles: replace SVG favicon with PNG and change welcome page title for clarity

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -4,7 +4,7 @@ import { Head } from '@inertiajs/react';
 export default function Welcome() {
     return (
         <>
-            <Head title="Welcome">
+            <Head title="Learn English">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
